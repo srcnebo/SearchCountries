d31c0407d400ffb5cb0ba96ff5758315efd21a77
Steven <stv.nebo@gmail.com>
2018-10-25 15:49:23 +0300

    Updates

---
+++ precache-manifest.6fb18d49b04072010eed67368e686f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5a43da572356bc1c930d35b4ce07729",
+    "url": "/SearchCountries/static/media/earth2.f5a43da5.jpg"
+  },
+  {
+    "revision": "9240f67310f863053e6b",
+    "url": "/SearchCountries/static/js/runtime~main.9240f673.js"
+  },
+  {
+    "revision": "55bbc0d544b09e48dd71",
+    "url": "/SearchCountries/static/js/main.55bbc0d5.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/SearchCountries/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "55bbc0d544b09e48dd71",
+    "url": "/SearchCountries/static/css/main.c941e77c.chunk.css"
+  },
+  {
+    "revision": "065daae1979612a1d10becdbad582450",
+    "url": "/SearchCountries/index.html"
+  }
+];
